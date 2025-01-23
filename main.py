510d0ff9eaf20a400a68c157de78c6ac870f5ef7
lasddawsadwda <Viper0501@hotmail.com>
2025-01-23 09:41:46 -0600

    Added time converter

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import assets, sys, time, os
 
-try: 
-    clockMode = sys.argv[1]
-except:
-    print("clockMode not specified, defaulting to 24h")
-    for x in range(5):
-        y = 5 - x
-        print(f"Continuing in {y}", end="\r")
-        time.sleep(1)
+def convertTime(timestamp):
+    valueList = {}
+    valueList["year"] = 1970
+    while timestamp > 31536000:
+        timestamp = timestamp - 31536000
+        valueList["year"] = valueList["year"] + 1
+    return valueList
+
+def getMode():
+    valid = ["-24", "-12"]
+    try: 
+        clockMode = sys.argv[1]
+        if clockMode not in valid:
+            raise ValueError("first arg must be '', '-12', or '-24'")
+    except:
+        print("clockMode not specified, defaulting to 24h")
+        
+        for x in range(5):
+            y = 5 - x
+            print(f"Continuing in {y}", end="\r")
+            time.sleep(1)
+    return clockMode
 
 
-while True:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    time = time.time()
+
+
+
     
